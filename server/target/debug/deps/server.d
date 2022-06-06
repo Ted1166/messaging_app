76ae0c85d59f4a82d2716509937a4be1ea3c805c
Ted1166 <ogolated00@gmail.com>
2022-06-06 17:37:50 +0300

    new version projec

--- server/target/debug/deps/server.d
+++ server/target/debug/deps/server.d
@@ -1,5 +1,5 @@
-C:\Users\User\Desktop\rust-project\messaging_app\server\target\debug\deps\server.exe: src\main.rs
+C:\Users\User\desktop\rust-project\messaging_app\server\target\debug\deps\server.exe: src\main.rs
 
-C:\Users\User\Desktop\rust-project\messaging_app\server\target\debug\deps\server.d: src\main.rs
+C:\Users\User\desktop\rust-project\messaging_app\server\target\debug\deps\server.d: src\main.rs
 
 src\main.rs:
